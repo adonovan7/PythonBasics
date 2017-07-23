f8080feb75f5a3640ff69a904e0309117cec180c
Andie Donovan <andiedonovan@yahoo.com>
2017-07-22 18:48:40 -0700

    Add files via upload

---
+++ Textbook/Exercises/PartA/ex5.py
@@ -0,0 +1,19 @@
+my_name = 'Andie'
+my_age = 20
+my_height = 67
+my_weight = 120
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'blond'
+
+print "Let's talk about %s." % my_name
+print "She's %d inches tall." % my_height
+print "She's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "She's got %s eyes and %s hair." % (my_eyes , my_hair)
+print "Her teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+
+# %r prints it no matter what its value is
+# %s, %r, %d are "formatters"
